ad3600d0de7a770902d8a063f3e6c1280290c4a2
anerthy <112020687+anerthy@users.noreply.github.com>
2025-10-15 20:02:56 -0600

    feat: add getProductBySlug action and update index to include it

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,12 +1,10 @@
 import { loginUser, logout, registerUser } from './auth';
-import { getProductsByPage } from './products/get-products-by-page.action';
+import { getProductsByPage, getProductBySlug } from './products';
 
 export const server = {
-  // actions
-
-  // Auth
   loginUser,
   logout,
   registerUser,
   getProductsByPage,
+  getProductBySlug,
 };
