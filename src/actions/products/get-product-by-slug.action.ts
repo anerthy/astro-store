767317ba2da35fca1254b02c93d9f73e768159bd
anerthy <112020687+anerthy@users.noreply.github.com>
2025-10-27 20:09:10 -0600

    feat: add new product initialization for slug 'new' in getProductBySlug action
    para cuando se crea un nuevo producto

--- src/actions/products/get-product-by-slug.action.ts
+++ src/actions/products/get-product-by-slug.action.ts
@@ -2,12 +2,33 @@ import { defineAction } from 'astro:actions';
 import { db, eq, Product, ProductImage } from 'astro:db';
 import { z } from 'astro:schema';
 
+const newProduct = {
+  id: '',
+  description: '',
+  images: '',
+  stock: 0,
+  price: 0,
+  sizes: '',
+  slug: '',
+  type: 'shirts',
+  tags: '',
+  title: '',
+  gender: 'men',
+};
+
 export const getProductBySlug = defineAction({
   accept: 'json',
   input: z.object({
     slug: z.string(),
   }),
   handler: async ({ slug }) => {
+    if (slug.trim() === 'new') {
+      return {
+        product: newProduct,
+        images: [],
+      };
+    }
+
     const [product] = await db
       .select()
       .from(Product)
